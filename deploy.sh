d7290c0438babb1e148345d08939a75bcc553336
sophie-in-the-cloud <dlagytjs20@gmail.com>
2025-12-27 14:20:45 +0900

    update sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+
 rsync -av --delete /tmp/deployment-archive/ /usr/share/nginx/html/
+rm -rf /tmp/deployment-archive/*
 
 chown -R nginx:nginx /usr/share/nginx/html/
 chmod -R 755 /usr/share/nginx/html/
